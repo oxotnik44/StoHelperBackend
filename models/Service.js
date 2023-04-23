ca5e35c54db2b81f26c70d5982b282c19a2f2038
oxotnik44 <artemnovikov1969@gmail.com>
2023-04-23 12:24:23 +0700

    изменил номер вацапп на имя админа

--- models/Service.js
+++ models/Service.js
@@ -5,7 +5,7 @@ const serviceSchema = new mongoose.Schema({
   login: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   nameService: { type: String, required: true, unique: true },
-  whatsappNumber: { type: String, required: true, unique: true },
+  nameAdmin: { type: String, required: true, unique: true },
   webAddress: { type: String, required: true },
   startOfWork: { type: String, required: true },
   endOfWork: { type: String, required: true },
